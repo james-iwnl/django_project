b7b4919d80325ef2578fe87a3c8bf55bfd7fc12c
james-iwnl <jjking.98@gmail.com>
2021-06-26 14:51:38 +0100

    template fix from snippets

--- blog/views.py
+++ blog/views.py
@@ -2,24 +2,26 @@ from django.shortcuts import render
 
 posts = [
     {
-        'author':'JamesK',
-        'title': 'Blog post 1',
-        'content':'First post content',
-        'date_posted': 'June 20, 2021'
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
     },
     {
-        'author':'Jane Doe',
-        'title': 'Blog post 2',
-        'content':'Second post content',
-        'date_posted': 'August, 2021'
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
     }
 ]
+
 
 def home(request):
     context = {
         'posts': posts
     }
     return render(request, 'blog/home.html', context)
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
