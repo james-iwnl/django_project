de93cffb429de8fc7d93bdc25f7ef358eff4ee2a
james-iwnl <jjking.98@gmail.com>
2021-06-26 15:12:04 +0100

    content update

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 posts = [
     {
-        'author': 'CoreyMS',
+        'author': 'James King',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'August 27, 2018'
+        'date_posted': 'June 26, 2021'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Adam Heeps',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
+        'date_posted': 'June 26, 2021'
     }
 ]
 
