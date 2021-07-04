6e5797dc7db1433dcd9d7e2e9803861082b49a47
james-iwnl <jjking.98@gmail.com>
2021-07-04 13:32:02 +0100

    create database and posts

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'James King',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'June 26, 2021'
-    },
-    {
-        'author': 'Adam Heeps',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'June 26, 2021'
-    }
-]
-
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
